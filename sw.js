d5a0340c8b281620845d6dbecfc82acbae1aab07
RVNEW2026 <rahulsandarbh@gmail.com>
2026-04-21 11:47:26 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker installed');
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
